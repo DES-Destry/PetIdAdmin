be1ab39f01e76b8119580c8fdffa6b3ddfdab61f
Andrey Kirik <dev.andrey.kirik@gmail.com>
2024-01-12 01:30:15 +0600

    fix: make report list prettier

--- pages/reports.vue
+++ pages/reports.vue
@@ -57,25 +57,29 @@ export default Vue.extend({
     <h1 class="page-title">Reports</h1>
     <b-tabs class="tabs" content-class="mt-3">
       <b-tab lazy title="Not resolved">
-        <report-card
-          v-for="report of notResolvedReports"
-          :key="report.id"
-          :created-at="report.createdAt"
-          :is-resolved="report.isResolved"
-          :reporter="report.reporter.username"
-          :tag-id="report.corruptedTag.id"
-        ></report-card>
+        <div class="report-list">
+          <report-card
+            v-for="report of notResolvedReports"
+            :key="report.id"
+            :created-at="report.createdAt"
+            :is-resolved="report.isResolved"
+            :reporter="report.reporter.username"
+            :tag-id="report.corruptedTag.id"
+          ></report-card>
+        </div>
       </b-tab>
       <b-tab lazy title="Resolved">
-        <report-card
-          v-for="report of resolvedReports"
-          :key="report.id"
-          :created-at="report.createdAt"
-          :is-resolved="report.isResolved"
-          :reporter="report.reporter.username"
-          :resolver="report.resolver.username"
-          :tag-id="report.corruptedTag.id"
-        ></report-card>
+        <div class="report-list">
+          <report-card
+            v-for="report of resolvedReports"
+            :key="report.id"
+            :created-at="report.createdAt"
+            :is-resolved="report.isResolved"
+            :reporter="report.reporter.username"
+            :resolver="report.resolver.username"
+            :tag-id="report.corruptedTag.id"
+          ></report-card>
+        </div>
       </b-tab>
     </b-tabs>
   </div>
@@ -99,5 +103,20 @@ export default Vue.extend({
 .tabs {
   width: 65vw;
   font-size: 32px;
+}
+
+.report-list {
+  margin-bottom: 50px;
+
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  justify-content: space-around;
+
+  margin-top: 50px;
+
+  * {
+    margin: 15px;
+  }
 }
 </style>
